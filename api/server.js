a9aa635cacdde820e35685ec331fa0848c16400c
Zag <za12ew44zz_999@hotmail.com>
2026-02-26 12:30:32 +0700

    add

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN || '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
-}));
+}));        
 
 // Body parser
 app.use(express.json());
